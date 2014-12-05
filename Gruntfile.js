9d121a34fb64efef88732a6efb8a520aab512990
Paulo McNally <paulomcnally@gmail.com>
2014-12-04 23:34:02 -0600

    fix loopback auto

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,8 @@ module.exports = function(grunt) {
           dataSource: 'db',
           app: './server/server',
           config: './server/model-config',
-          method: 'autoupdate'
+          method: 'autoupdate',
+          exclude: ['User']
         }
       }
     }
